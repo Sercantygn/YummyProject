7b6f07975b65b9b6c11dca16ea65fe5dd3d37f3b
Sercan TOYGUN <sercan.toygun41@gmail.com>
2024-12-12 21:49:13 +0300

    Add project files.

---
+++ YummyProject/Models/ContanctInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YummyProject.Models
+{
+    public class ContanctInfo
+    {
+        public int ContactInfoId { get; set; }
+        public string Address { get; set; }
+        public string EMail { get; set; }
+        public string PhoneNumber { get; set; }
+        public string MapUrl { get; set; }
+        public string OpenHours { get; set; }
+    }
+}
